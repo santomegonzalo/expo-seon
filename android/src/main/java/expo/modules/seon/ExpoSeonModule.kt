40927150c1c9a9c0d1db962927fc53e942e03195
Gonzalo Santome <santomegonzalo@gmail.com>
2023-03-14 13:26:44 +0000

    feat(android): support seon on android

--- android/src/main/java/expo/modules/seon/ExpoSeonModule.kt
+++ android/src/main/java/expo/modules/seon/ExpoSeonModule.kt
@@ -1,25 +1,32 @@
 package expo.modules.seon
 
+import expo.modules.kotlin.Promise
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import io.seon.androidsdk.service.SeonBuilder
 
 class ExpoSeonModule : Module() {
   // Each module class must implement the definition function. The definition consists of components
   // that describes the module's functionality and behavior.
   // See https://docs.expo.dev/modules/module-api for more details about available components.
-  override fun definition() = ModuleDefinition {
+  override fun definition() = ModuleDefinition {    
     // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
     // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
     // The module will be accessible from `requireNativeModule('ExpoSeon')` in JavaScript.
     Name("ExpoSeon")
 
     AsyncFunction("fingerprintBase64") { sessionId: String, promise: Promise ->
-      val sfp = SeonBuilder().withContext(applicationContext).withSessionId(sessionID).build()
+      val activity = appContext.activityProvider?.currentActivity
+      val applicationContext = activity?.applicationContext
 
-      sfp.setLoggingEnabled(true)
+      if (applicationContext != null) {
+        val sfp = SeonBuilder().withContext(applicationContext).withSessionId(sessionId).build()
 
-      sfp.getFingerprintBase64 { seonFingerprint: String? ->
-        promise.resolve(seonFingerprint)
+        sfp.getFingerprintBase64 { seonFingerprint: String? ->
+          promise.resolve(seonFingerprint)
+        }
+      } else {
+        promise.reject("500", "ApplicationContext is not valid", null);
       }
     }
   }
